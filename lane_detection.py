bb0cbff9b0c7e1bdc04ed2d33e075139449774e5
TotallyNotAPumpkin <leopardcloudew@gmail.com>
2023-07-26 16:59:06 -0400

    docstrings added

--- lane_detection.py
+++ lane_detection.py
@@ -4,6 +4,17 @@ import matplotlib.pyplot as plt
 import math
 
 def detect_lines(img, threshold1 = 50, threshold2 = 150, apertureSize = 3, minLineLength = 100, maxLineGap = 10):
+    """Detects if lines are present in an image (pool).
+    args:
+        img (image path or np.ndarray): image of pool to detect lines from
+        threshold1 (int): lower threshold to detect lines (default 50)
+        threshold2 (int): upper threshold to detect lines (default 150)
+        apertureSize (odd int): amount of details cv2 will be taking (default 3)
+        minLineLength (int): the minimum line length of lines detected (default 100)
+        maxLineGap (int): the maximum line gap of lines detected (default 10)
+    returns:
+        (list): list of points [[x1, y2, x2, y2], ...] that correspond to detected lines
+    """
     image = cv2.imread(img)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert to grayscale
     grayCon = cv2.addWeighted(gray, 2, gray, 0, 0)
@@ -27,12 +38,27 @@ def detect_lines(img, threshold1 = 50, threshold2 = 150, apertureSize = 3, minLi
     return lineList
 
 def draw_lines(img, lines, color = (0, 255, 0)):
+    """Returns an image with specified lines drawn on
+    args:
+        img (image path or np.ndarray): image that lines are drawn on
+        lines (list): list of points [[x1, y2, x2, y2], ...] of the lines being drawn
+        color (tuple): (x, x, x)
+    returns:
+        (np.ndarray): image with lines drawn on
+    """
     image = cv2.imread(img)
     for line in lines:
         cv2.line(image, (line[0], line[1]), (line[2], line[3]), color, 3) 
     return image
 
 def get_slopes_intercepts(img, lines):
+    """Returns slopes and bottom x-intercepts of given lines in an image
+    args: 
+        img (image path or np.ndarray): image that lines reside in
+        lines (list): list of points [[x1, y2, x2, y2], ...] of the lines being drawn
+    returns:
+        (list, list): 2 lists of the slopes and intercepts of the specified lines
+        """
     image = img
     slopes = []
     intercepts = []
@@ -44,6 +70,9 @@ def get_slopes_intercepts(img, lines):
     return slopes, intercepts
 
 def detect_lanes(imageInput, lines):
+    """Detects lanes from given lines and an image.
+    imageInput (image path or np.ndarray): 
+    """
     image = cv2.imread(imageInput)
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     slopes, intercepts = get_slopes_intercepts(img, lines)
